67233c4243b404d740eb5caea8cbd0a4479fceed
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2022-11-06 19:41:39 +0100

    create login and signup API

---
+++ src/main/java/com/example/reddit/service/AuthService.java
@@ -0,0 +1,86 @@
+package com.example.reddit.service;
+
+import com.example.reddit.dto.AuthenticationResponse;
+import com.example.reddit.dto.LoginRequest;
+import com.example.reddit.dto.RegisterRequest;
+import com.example.reddit.exception.SpringRedditException;
+import com.example.reddit.model.NotificationEmail;
+import com.example.reddit.model.RedditUser;
+import com.example.reddit.model.VerificationToken;
+import com.example.reddit.repository.RedditUserRepository;
+import com.example.reddit.repository.VerificationTokenRepository;
+import com.example.reddit.security.JwtProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final RedditUserRepository redditUser;
+    private final MailService mailService;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        RedditUser user = new RedditUser();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+        redditUser.save(user);
+
+        String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please Activate your account",
+                user.getEmail(),
+                "Thank you for Signing up click to the url below : "+
+                        "http://localhost:8089/api/auth/accountVerification/"+token
+                ));
+
+    }
+
+    private String generateVerificationToken(RedditUser user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken  = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(()-> new SpringRedditException("Invalid Token"));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        RedditUser user = redditUser.findByUsername(username).orElseThrow(()->new SpringRedditException("user not found"));
+        user.setEnabled(true);
+        redditUser.save(user);
+    }
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtProvider.generateToken(authentication);
+        return new AuthenticationResponse(token,loginRequest.getUsername());
+    }
+}
